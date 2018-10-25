75a941c6674f8b162ab3a921d962ee5327c4b4bc
licerlee <licerlee@gmail.com>
2018-10-25 11:26:37 +0800

    完善基础配置中心

--- config-client/src/main/java/com/licerlee/configclient/ConfigClientApplication.java
+++ config-client/src/main/java/com/licerlee/configclient/ConfigClientApplication.java
@@ -1,12 +1,32 @@
 package com.licerlee.configclient;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class ConfigClientApplication {
 
 	public static void main(String[] args) {
 		new SpringApplicationBuilder(ConfigClientApplication.class).web(true).run(args);
+	}
+
+	@Value("${info.profile}") // git配置文件里的key
+	String profile;
+	
+	@Value("${info.from}") // git配置文件里的key
+	String from;
+
+	@RequestMapping(value = "/profile")
+	public String profile() {
+		return profile;
+	}
+	
+	@RequestMapping(value = "/from")
+	public String from() {
+		return from;
 	}
 }
