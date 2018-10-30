7c251bed78c501ff556080ab1183665cc096ded4
licerlee <licerlee@gmail.com>
2018-10-30 14:44:24 +0800

    sleuth分布式追踪基础

---
+++ trace-2/src/main/java/com/licerlee/trace2/Trace2Application.java
@@ -0,0 +1,28 @@
+package com.licerlee.trace2;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@EnableDiscoveryClient
+@SpringBootApplication
+public class Trace2Application {
+
+	private final Logger logger = LoggerFactory.getLogger(getClass());
+
+	@RequestMapping(value = "/trace-2", method = RequestMethod.GET)
+	public String trace() {
+		logger.info("===<call trace-2>===");
+		return "Trace";
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(Trace2Application.class, args);
+	}
+}
